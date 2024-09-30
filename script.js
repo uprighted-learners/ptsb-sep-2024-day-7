baf4a9a162d434cea2a8e85713197e5071643e09
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-09-30 18:41:00 -0500

    expressions

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const fruit = "berry"
 
 console.log(color)
 console.log(fruit)
+
+console.log(color + fruit)
+
+let ourFruit = (color + fruit).toUpperCase()
+console.log(ourFruit)
+
+if (color) {
+    console.log("we have a color: " + color)
+} else {
+    console.log("we don't have a color!")
+}
